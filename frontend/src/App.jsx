ef765429b471a3a444fa4dd9d444796c4fd79162
pravallika1120 <upravallika75@gmail.com>
2026-07-11 21:31:41 +0530

    Added login functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Students from "./pages/Students";
+import Students from "./pages/StudentList.jsx";
 
 
 function App(){
