3d797d1f560da2be0dc96ef7c99511e2f219d119
UPPALAPATI PRAVALLIKA <upravallika75@gmail.com>
2026-07-12 20:32:14 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,28 +1,33 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true
+
+const userSchema = new mongoose.Schema({
+
+    name:{
+        type:String,
+        required:true
     },
 
-    password: {
-      type: String,
-      required: true
+    email:{
+        type:String,
+        required:true,
+        unique:true
     },
 
-    role: {
-      type: String,
-      enum: ["admin", "student"],
-      default: "student"
+    password:{
+        type:String,
+        required:true
+    },
+
+    role:{
+        type:String,
+        default:"student"
     }
-  },
-  {
-    timestamps: true
-  }
-);
+
+},
+{
+    timestamps:true
+});
 
 
 module.exports = mongoose.model("User", userSchema);
