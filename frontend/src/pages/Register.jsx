a6bf3c55e059b0e77ba718de347c9cb16cc14ed2
UPPALAPATI PRAVALLIKA <upravallika75@gmail.com>
2026-07-12 20:36:55 +0530

    Create Register.jsx

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,199 @@
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import API from "../api/studentApi";
+
+
+function Register(){
+
+
+const navigate=useNavigate();
+
+
+const [user,setUser]=useState({
+
+name:"",
+email:"",
+password:"",
+role:"student"
+
+});
+
+
+
+const handleChange=(e)=>{
+
+
+setUser({
+
+...user,
+
+[e.target.name]:e.target.value
+
+});
+
+
+};
+
+
+
+const handleSubmit=async(e)=>{
+
+
+e.preventDefault();
+
+
+try{
+
+
+await API.post("/auth/register",user);
+
+
+alert("Registration Successful");
+
+
+navigate("/");
+
+
+}
+catch(error){
+
+
+alert(
+error.response?.data?.message ||
+"Registration Failed"
+);
+
+
+}
+
+
+};
+
+
+
+
+return(
+
+<div className="container mt-5">
+
+
+<div className="card shadow">
+
+
+<div className="card-header bg-primary text-white">
+
+<h3>
+Student Registration
+</h3>
+
+</div>
+
+
+
+<div className="card-body">
+
+
+<form onSubmit={handleSubmit}>
+
+
+<input
+
+className="form-control mb-3"
+
+name="name"
+
+placeholder="Enter Name"
+
+onChange={handleChange}
+
+/>
+
+
+
+<input
+
+className="form-control mb-3"
+
+name="email"
+
+type="email"
+
+placeholder="Enter Email"
+
+onChange={handleChange}
+
+/>
+
+
+
+<input
+
+className="form-control mb-3"
+
+name="password"
+
+type="password"
+
+placeholder="Enter Password"
+
+onChange={handleChange}
+
+/>
+
+
+
+<select
+
+className="form-control mb-3"
+
+name="role"
+
+onChange={handleChange}
+
+>
+
+
+<option value="student">
+
+Student
+
+</option>
+
+
+<option value="admin">
+
+Admin
+
+</option>
+
+
+</select>
+
+
+
+<button className="btn btn-success">
+
+Register
+
+</button>
+
+
+</form>
+
+
+</div>
+
+
+</div>
+
+
+</div>
+
+
+);
+
+
+}
+
+
+export default Register;
