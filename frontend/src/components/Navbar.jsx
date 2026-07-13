361d8a20f344b9959d9edd2443c05662a71865d3
UPPALAPATI PRAVALLIKA <upravallika75@gmail.com>
2026-07-13 13:43:53 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,29 +1,106 @@
-import {useEffect,useState} from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 function Navbar(){
 
-const [name,setName]=useState("");
+    const [name,setName] = useState("");
+    const [showProfile,setShowProfile] = useState(false);
+
+    const navigate = useNavigate();
 
 
-useEffect(()=>{
+    useEffect(()=>{
 
-const username =
-localStorage.getItem("name");
+        const username = localStorage.getItem("name");
 
-setName(username);
+        setName(username);
 
-},[]);
-
+    },[]);
 
 
-return(
 
-<h4>
-Welcome, {name}
-</h4>
+    const logout = () => {
 
-);
+        localStorage.clear();
+
+        navigate("/");
+
+    };
+
+
+    return(
+
+        <nav className="navbar navbar-light bg-light px-4 shadow">
+
+            <h4>
+                Welcome, {name}
+            </h4>
+
+
+            <div className="position-relative">
+
+
+                <button
+                    className="btn btn-outline-primary"
+                    onClick={()=>setShowProfile(!showProfile)}
+                >
+                    👤 Profile
+                </button>
+
+
+
+                {
+                    showProfile && (
+
+                        <div 
+                        className="card position-absolute p-3"
+                        style={{
+                            right:0,
+                            width:"220px",
+                            zIndex:1000
+                        }}
+                        >
+
+                            <h5>
+                                {localStorage.getItem("name")}
+                            </h5>
+
+
+                            <p>
+                                Email:
+                                <br/>
+                                {localStorage.getItem("email")}
+                            </p>
+
+
+                            <p>
+                                Role:
+                                <br/>
+                                {localStorage.getItem("role")}
+                            </p>
+
+
+                            <button
+                            className="btn btn-danger"
+                            onClick={logout}
+                            >
+                                Logout
+                            </button>
+
+
+                        </div>
+
+                    )
+                }
+
+
+            </div>
+
+
+        </nav>
+
+    );
 
 }
 
